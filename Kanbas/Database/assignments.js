178f605990627d7bcb86dd3e3ff4d5c8292704d3
Spec-DY <ibonehp@gmail.com>
2024-08-06 23:56:32 -0700

    add: kanbas data

---
+++ Kanbas/Database/assignments.js
@@ -0,0 +1,92 @@
+export default [
+    {
+        "_id": "A101",
+        "title": "Propulsion Assignment",
+        "course": "RS101",
+        "description": "Analyze the principles of rocket propulsion.",
+        "points": 100,
+        "dueDate": "2024-05-13",
+        "availableFrom": "2024-05-06",
+        "availableUntil": "2024-05-20"
+    },
+    {
+        "_id": "A102",
+        "title": "Combustion Analysis",
+        "course": "RS101",
+        "description": "Evaluate different combustion processes in rockets.",
+        "points": 100,
+        "dueDate": "2024-05-20",
+        "availableFrom": "2024-05-13",
+        "availableUntil": "2024-05-27"
+    },
+    {
+        "_id": "A103",
+        "title": "Nozzle Design Project",
+        "course": "RS101",
+        "description": "Design an optimal nozzle for a given rocket engine.",
+        "points": 100,
+        "dueDate": "2024-05-27",
+        "availableFrom": "2024-05-20",
+        "availableUntil": "2024-06-03"
+    },
+    {
+        "_id": "A201",
+        "title": "Aerodynamics Quiz",
+        "course": "RS102",
+        "description": "Test your knowledge on basic aerodynamic principles.",
+        "points": 50,
+        "dueDate": "2024-05-13",
+        "availableFrom": "2024-05-06",
+        "availableUntil": "2024-05-20"
+    },
+    {
+        "_id": "A202",
+        "title": "Flow Analysis",
+        "course": "RS102",
+        "description": "Analyze the flow dynamics of various airfoils.",
+        "points": 100,
+        "dueDate": "2024-05-20",
+        "availableFrom": "2024-05-13",
+        "availableUntil": "2024-05-27"
+    },
+    {
+        "_id": "A203",
+        "title": "Heating Analysis",
+        "course": "RS102",
+        "description": "Evaluate thermal protection systems for high-speed aircraft.",
+        "points": 100,
+        "dueDate": "2024-05-27",
+        "availableFrom": "2024-05-20",
+        "availableUntil": "2024-06-03"
+    },
+    {
+        "_id": "A301",
+        "title": "Structural Design Task",
+        "course": "RS103",
+        "description": "Design the structural components of a spacecraft.",
+        "points": 100,
+        "dueDate": "2024-05-13",
+        "availableFrom": "2024-05-06",
+        "availableUntil": "2024-05-20"
+    },
+    {
+        "_id": "A302",
+        "title": "Orbital Calculations",
+        "course": "RS103",
+        "description": "Calculate the orbits of different spacecraft.",
+        "points": 100,
+        "dueDate": "2024-05-20",
+        "availableFrom": "2024-05-13",
+        "availableUntil": "2024-05-27"
+    },
+    {
+        "_id": "A303",
+        "title": "Systems Engineering Exam",
+        "course": "RS103",
+        "description": "Test your knowledge on spacecraft systems engineering.",
+        "points": 100,
+        "dueDate": "2024-05-27",
+        "availableFrom": "2024-05-20",
+        "availableUntil": "2024-06-03"
+    }
+]
