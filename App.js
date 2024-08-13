6e1f668ebda51054a2ada3fff5cd6d5776a6d99c
Spec-DY <ibonehp@gmail.com>
2024-08-12 17:58:22 -0700

    3.6

--- App.js
+++ App.js
@@ -10,12 +10,15 @@ import AssignmentsRoutes from './Kanbas/Assignments/routes.js';
 import mongoose from "mongoose";
 import "dotenv/config";
 
+import UserRoutes from "./Users/routes.js";
+
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 
 CourseRoutes(app);
 ModuleRoutes(app);
