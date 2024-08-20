96fa19925ee7e131913a7f48fa0888faddd3f027
Spec-DY <ibonehp@gmail.com>
2024-08-19 22:44:43 -0700

    add: quiz style

--- Kanbas/Quiz/schema.js
+++ Kanbas/Quiz/schema.js
@@ -10,14 +10,14 @@ const quizSchema = new mongoose.Schema({
     timeLimit: { type: Number, default: 20 },  // in minutes
     multipleAttempts: { type: Boolean, default: false },
     numberOfAttempts: { type: Number, default: 1 },  // only relevant if multipleAttempts is true
-    showCorrectAnswers: { type: String },  // can be a date or a boolean
+    showCorrectAnswers: { type: Boolean, default: true },  // can be a date or a boolean
     accessCode: { type: String },
     oneQuestionAtATime: { type: Boolean, default: true },
     webcamRequired: { type: Boolean, default: false },
     lockQuestionsAfterAnswering: { type: Boolean, default: false },
-    dueDate: { type: Date },
-    availableDate: { type: Date },
-    untilDate: { type: Date },
+    dueDate: { type: Date, default: 2024-8-31 },
+    availableDate: { type: Date, default: 2024-8-10 },
+    untilDate: { type: Date, default: 2024-9-10 },
     questions: [{ type: mongoose.Schema.Types.ObjectId, ref: "QuestionModel" }],
     isPublished: { type: Boolean, default: false }
 }, { collection: "quiz" });
